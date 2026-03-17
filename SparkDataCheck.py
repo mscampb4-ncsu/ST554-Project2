4ba3c3a4cde1dd0aa97e96402d4a1fb07dbc62ef
mscampb4-ncsu <mscampb4@ncsu.edu>
2026-03-17 15:25:45 -0400

    Initial commit.

---
+++ SparkDataCheck.py
@@ -0,0 +1,10 @@
+from pyspark.sql import DataFrame
+from pyspark.sql import functions as F
+from functools import reduce
+from pyspark.sql.types import *
+import pandas as pd
+
+class SparkDataCheck:
+    
+    def__init(self, df):
+        self.df = df
