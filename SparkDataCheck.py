fb4021cc9cf97b4c8cfe0a49538b797db98ff48f
mscampb4-ncsu <mscampb4@ncsu.edu>
2026-03-18 15:34:33 -0400

    Implement two `@classmethod`s

--- SparkDataCheck.py
+++ SparkDataCheck.py
@@ -6,5 +6,35 @@ import pandas as pd
 
 class SparkDataCheck:
     
-    def__init(self, df):
+    def __init__(self, df):
+        """
+        Initialize SparkDataCheck with one attribute:
+        df - a dataframe
+        """
         self.df = df
+
+    @classmethod
+    def load_pyspark(self, spark, filePath: str):
+        """
+        Loads the class via spark using the following parameters:
+        self - this instance of SparkDataCheck
+        spark - a SparkSession instance
+        filePath - a string file path to the destination file.
+        """
+        df = spark.read.format("csv").load(filePath)
+        sdc = self(df)
+        return sdc
+    
+    @classmethod
+    def load_pandas(self, spark, pandasDf):
+        """
+        Loads the class via spark using the following parameters:
+        self - this instance of SparkDataCheck
+        spark - a SparkSession instance
+        pandasDf - a pandas DataFrame
+        """
+        df = spark.createDataFrame(pandasDf)
+        sdc = self(df)
+        return sdc
+    
+        
